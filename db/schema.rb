c3e7a25874c7fd937ea9e879110dc8bf4ef81e84
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-08-24 09:41:27 +0545

    add admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_024559) do
+ActiveRecord::Schema.define(version: 2021_08_24_035454) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_024559) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
