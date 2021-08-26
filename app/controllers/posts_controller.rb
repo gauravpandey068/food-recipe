e852bdaf320e3cf23c62fa8c364b3a4d9cdcb633
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-08-26 09:45:05 +0545

    remove unuse bl0ck

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
- # before_action :correct_user, only: [:create, :edit, :update, :show]
   before_action :is_admin?, only: [:new , :create, :edit, :update, :destroy]
   # GET /posts or /posts.json
   def index
