165cad961d6616e34d5e1685b0e68593aa90a714
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-08-26 09:45:18 +0545

    add validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :image, presence: true
+    validates :description, presence: true
+    validates :category, presence: true
+    validates :ingredients, presence: true
+    validates :method, presence: true
+    validates :prep, presence: true
+    validates :cook, presence: true
+    validates :difficult, presence: true
+
 end
